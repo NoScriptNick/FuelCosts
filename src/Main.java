e7f21c5255c246bdaea3736612dc830f8c5964e5
613668 <beres.613668@swl.k12.oh.us>
2023-10-10 09:41:07 -0400

    dfhdfhdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //variables
+        double gallonsInTank;
+        double milePerGallon;
+        double pricePerGallon;
+        boolean doneOne = false;
+        boolean doneTwo = false;
+        boolean doneThree = false;
+
+        //asks user for amount of gallons in their tank currently
+        do {
+            System.out.println("Enter the amount of gallons in your tank currently: ");
+            if (scan.hasNextDouble()) {
+                gallonsInTank = scan.nextDouble();
+                if (gallonsInTank <= 0) {
+                    scan.nextLine();
+                } else {
+                    doneOne = true;
+                }
+            } else {
+                scan.nextLine();
+            }
+        } while (!doneOne);
+
+        //asks user for mpg for their car
+        do {
+            System.out.println("Enter your car's fuel efficiency: ");
+            if (scan.hasNextDouble()) {
+                gallonsInTank = scan.nextDouble();
+                if (gallonsInTank <= 0) {
+                    scan.nextLine();
+                } else {
+                    doneOne = true;
+                }
+            } else {
+                scan.nextLine();
+            }
+        } while (!doneOne);
+
+
     }
 }
