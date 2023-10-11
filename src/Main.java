85d46f922c0144daa9cd7ef256575543a8f0c9e4
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-10-10 21:15:27 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         //variables
-        double gallonsInTank;
-        double milePerGallon;
-        double pricePerGallon;
+        double gallonsInTank = 0;
+        double mpg = 0;
+        double pricePerGallon = 0;
         boolean doneOne = false;
         boolean doneTwo = false;
         boolean doneThree = false;
+        double hundredMileCost = 0;
+        double distance = 0;
 
         //asks user for amount of gallons in their tank currently
         do {
@@ -28,19 +30,39 @@ public class Main {
 
         //asks user for mpg for their car
         do {
-            System.out.println("Enter your car's fuel efficiency: ");
+            System.out.println("Enter your car's fuel efficiency in MPG: ");
             if (scan.hasNextDouble()) {
-                gallonsInTank = scan.nextDouble();
-                if (gallonsInTank <= 0) {
+                mpg = scan.nextDouble();
+                if (mpg < 0) {
                     scan.nextLine();
                 } else {
-                    doneOne = true;
+                    doneTwo = true;
                 }
             } else {
                 scan.nextLine();
             }
-        } while (!doneOne);
+        } while (!doneTwo);
 
+        //asks user for price per gallon
+        do {
+            System.out.println("Enter current price per gallon: ");
+            if (scan.hasNextDouble()) {
+                pricePerGallon = scan.nextDouble();
+                if (pricePerGallon < 0) {
+                    scan.nextLine();
+                } else {
+                    doneThree = true;
+                }
+            } else {
+                scan.nextLine();
+            }
+        } while (!doneThree);
 
+        //prints cost for 100 miles and how far the car can go with the gas that is currently in the tank
+        //output should have only 2 decimal places
+        hundredMileCost = 100/mpg * (pricePerGallon);
+        distance = mpg * gallonsInTank;
+        System.out.printf("%-10s %.2f" , "Cost per 100 miles in $: " , hundredMileCost);
+        System.out.printf("\n%10s %.2f" , "Distance left in miles: " , distance);
     }
 }
